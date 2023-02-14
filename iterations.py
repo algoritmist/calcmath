702019338030053cd67d434c70f4254631c03841
viacheslav <barash.2018@yandex.ru>
2023-02-14 20:28:25 +0300

    changed variable names & added TODOs

--- iterations.py
+++ iterations.py
@@ -2,45 +2,45 @@ import numpy as np
 
 
 # solve Ax = b with the method of simple iterations
-def solve(A, b, delta):
+def solve(a, b, delta):
     alpha, beta = None, None
-    if check(A):
-        alpha, beta = to_iterative(A, b)
+    if check(a):
+        alpha, beta = to_iterative_form(a, b)
     else:
-        if det(A) != 0:
-            A, b = transform(A, b)
-            print(b)
-            if check(A):
-                alpha, beta = to_iterative(A, b)
+        if det(a) != 0:
+            a_transformed, b_transformed = transform(a, b)
+            alpha, beta = to_iterative_form(a_transformed, b_transformed)
 
+    #TODO: use Maybe
     if alpha is None or beta is None:
         return [], False
     return steps(alpha, beta, delta), True
 
 
 # Ax = b => x = beta + alpha * x
-def to_iterative(A, b):
-    alpha, beta = A, b
-
-    for i in range(len(A)):
+def to_iterative_form(a, b):
+    alpha, beta = a, b
+    for i in range(len(a)):
+        #TODO: random const
         const = 0
-        diag1 = A[i, i] + const
+        diag1 = a[i, i] + const
         diag2 = -const
         beta[i] = b[i] / diag1
-        for j in range(len(A)):
+        for j in range(len(a)):
             alpha[i, j] = -diag2 / diag1 if i == j else -A[i, j] / diag1
     return alpha, beta
 
 
 # if det A != 0, try to get iterative form immediately
-def transform(A, b):
-    if det(A) == 0:
-        return A, b
+def transform(a, b):
+    if det(a) == 0:
+        return a, b
     mul = np.vectorize(lambda x: 1e-6 * x)
-    eps = mul(np.random.rand(len(A), len(A)))
-    inv = np.linalg.inv(A)
+    eps = mul(np.random.rand(len(a), len(a)))
+    inv = np.linalg.inv(a)
     mult = np.add(inv, eps)
-    return np.matmul(mult, A), np.squeeze(np.asarray(np.matmul(mult, b)))
+    # TODO: try simplifying b multiplication
+    return np.matmul(mult, a), np.squeeze(np.asarray(np.matmul(mult, b)))
 
 
 # check if |Aii| > sum (Aij)
@@ -69,6 +69,7 @@ def step(alpha, beta, xs, delta):
 
 def steps(alpha, beta, delta):
     x = beta  # initial iteration
+    #TODO: iteration count
     while True:
         x, change = step(alpha, beta, x, delta)
         if not change:
--- main.py
+++ main.py
@@ -4,4 +4,8 @@ import numpy as np
 A = np.matrix('4 0.24 -0.08; 0.09 3 -0.15; 0.04 -0.08 4.0')
 b = np.array([8, 9, 20])
 
-print(solve(A, b, 1e-3))
+result = solve(A, b, 1e-3)
+if result[1]:
+    print(list(result[0]))
+else:
+    print("No solution")
