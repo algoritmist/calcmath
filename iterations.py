79640029daf2e39b4284951a9b8c971385f60e18
Viacheslav Baranov <barash.2018@yandex.ru>
2023-02-27 18:29:07 +0300

    removed unnecessary comments

--- iterations.py
+++ iterations.py
@@ -5,7 +5,6 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import maximum_bipartite_matching
 
 
-# solve Ax = b with the method of simple iterations
 def get_shuffled_row_convergence(a):
     rows = [[] for i in range(len(a))]
     for i in range(len(a)):
@@ -21,9 +20,6 @@ def transform(a, shuffle):
 
 def solve(a, b, delta):
     alpha, beta = None, None
-    # if is_has_convergence(a):
-    #    alpha, beta = to_iterative_form(a, b)
-    # else:
     shuffle = get_shuffled_row_convergence(a)
     if shuffle is not None:
         a_transformed, b_transformed = transform(a, shuffle), transform(b, shuffle)
@@ -36,38 +32,19 @@ def solve(a, b, delta):
     return Right((x, iterations))
 
 
-# Ax = b => x = beta + alpha * x
 def to_iterative_form(a, b):
     alpha, beta = a, b
     for i in range(len(a)):
-        # TODO: random const
-        const = 0
-        diag1 = a[i, i] + const
-        diag2 = -const
-        beta[i] = b[i] / diag1
+        diag = a[i, i]
+        beta[i] = b[i] / diag
         for j in range(len(a)):
-            alpha[i, j] = -diag2 / diag1 if i == j else -a[i, j] / diag1
+            alpha[i, j] = 0 if i == j else -a[i, j] / diag
     return alpha, beta
 
 
-# if det A != 0, try to get iterative form immediately
-# def transform(a, b):
-#    if det(a) == 0:
-#        return a, b
-#    mul = np.vectorize(lambda x: 1e-6 * x)
-#    eps = mul(np.random.rand(len(a), len(a)))
-#    inv = np.linalg.inv(a)
-#    mult = np.add(inv, eps)
-#    # TODO: try simplifying b multiplication
-#    return np.matmul(mult, a), np.squeeze(np.asarray(np.matmul(mult, b)))#
-
-
-# check if |Aii| > sum (Aij)
-def is_has_convergence(A):
-    to_abs = np.vectorize(lambda x: abs(x))
-    a = to_abs(A)
-    for row in range(len(a)):
-        if not is_convergence_satisfied(a, row, row):
+def is_has_convergence(matrix):
+    for row in range(len(matrix)):
+        if not is_convergence_satisfied(matrix, row, row):
             return False
     return True
 
@@ -91,15 +68,10 @@ def step(alpha, beta, xs, delta):
 
 
 def steps(alpha, beta, delta):
-    x = beta  # initial iteration
+    x = beta
     iterations = 0
     while True:
         x, change = step(alpha, beta, x, delta)
         iterations += 1
         if not change:
             return x, iterations
-
-
-# calculate the determinant of matrix
-def det(a):
-    return np.linalg.det(a)
