6b4dd9351e98c72cd0ba0c20c1eb313cfce8bff1
Viacheslav Baranov <barash.2018@yandex.ru>
2023-03-28 16:37:27 +0300

    added unresolvable integral for Newton-Leibniz formula

--- integrals/simpson/default_integrals.py
+++ integrals/simpson/default_integrals.py
@@ -7,8 +7,9 @@ x = symbols('x')
 
 integrals = [
     Integral(x ** 3, (0, 1), Right(1 / 4)),
-    #Integral(3 * atan(x) ** 4 / (1 + x ** 2), (-pi / 2, pi), Right(4.803684413)),
+    # Integral(3 * atan(x) ** 4 / (1 + x ** 2), (-pi / 2, pi), Right(4.803684413)),
     Integral((3 * x - 1) / sqrt(2 * x ** 2 - x + 1), (-5, 10), Right(8.4176339772)),
     Integral(1 / x, (-2, 2), Left("No solution")),
     Integral(sin(x) / x, (-2, 2), Left("No solution")),
+    Integral(exp(-x ** 2), (0, 1), Right(0.7468))
 ]
